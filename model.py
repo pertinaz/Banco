72ce9cb18563b7d035710c0aeec5410dde0baeca
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-03-20 09:43:52 -0500

    Update model.py
    Updating some methods and implementing the program in the main using the ui module tools

--- model.py
+++ model.py
@@ -1,27 +1,33 @@
 class bankAccount:
+    accountList = []
     def __init__(self, accountNumber,userId,userName,currentBalance):
         self.accountNumber = accountNumber
         self.userId= userId
         self.userName = userName
         self.currentBalance = currentBalance
 
-    def newAccount(accountList,userName,userId,accountNumber,currentBalance):    
-        for account in accountList:
+        bankAccount.accountList.append(self)
+
+    @classmethod
+    def newAccount(cls,userName,userId,accountNumber,currentBalance):    
+        for account in cls.accountList:
             if userId == account.userId:
                 print("Usuario ya registrado...")
                 return
-        accountList.append(bankAccount(accountNumber,userId,userName,currentBalance))
-      
-    def ListOfAccounts(accountList):
-        for account in accountList:
+        cls.accountList.append(bankAccount(accountNumber,userId,userName,currentBalance))
+                
+    @classmethod
+    def ListOfAccounts(cls):
+        for account in cls.accountList:
             print(f"Name: {account.userName}")
             print(f"ID: {account.userId}")
             print(f"Account Number: #{account.accountNumber}")
             print(f"Balance: ${account.currentBalance}")
             print()
 
-    def showUser(accountList,userId):
-        for account in accountList:
+    @classmethod
+    def showUser(cls,userId):
+        for account in cls.accountList:
             if userId == account.userId:
                 print(f"Name: {account.userName}")
                 print(f"ID: {account.userId}")
@@ -29,12 +35,28 @@ class bankAccount:
                 print(f"Balance: ${account.currentBalance}")
                 return
             print("Usuario no encontrado.")
-          
+
+
+    # METODOS PRIVAODS
+    @classmethod        
+    def accountValidation(cls,accountNumber):
+        if not cls.accountList:
+            print("No se encuentra la información.")
+        else:
+            accountFound = False
+            for account in cls.accountList:   
+                if accountNumber == account.accountNumber:
+                    print(f"Bienvenido {account.userName}")
+                    accountFound = True
+            if not accountFound:
+                print("Numero de cuenta incorrecto.") 
+                    
+
     def addMoney(self,amount):
         self.currentBalance += amount
         print(f"Nuevo saldo: ${self.currentBalance}")
     
-    
+
     def withdrawMoney(self,amount):
         if amount > self.currentBalance:
             print("No hay fondos suficientes")
